6e8ff668353d14b02d0e4242d3339ef4f0dc85d5
Hanjala Khan <khanhanjala811@gmail.com>
2026-01-18 10:52:06 +0530

    ui changed

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,41 @@ import TechFaq from './components/TechFaq'
 import PaymentFaq from './components/PaymentFaq'
 import IosFaq from './components/IosFaq'
 import AndroidFaq from './components/AndriodFaq'
+import News from './dropdown/News'
+import Blogs from './dropdown/Blogs'
+import Infographics from './dropdown/Infographics'
+import Testimonials from './dropdown/Testimonials'
+import Pagination from './components/Pagination'
 
 const App = () => {
   const [data, setData] = useState([...items])
   const [cart, setCart] = useState([])
 
   return (
-    <>
-      <Router>
-        <Navbar cart={cart} setData={setData} />
-        <Routes>
-          <Route path="/" element={<Product cart={cart} setCart={setCart} items={data} />} />
-          <Route path="/product/:id" element={<ProductDetail cart={cart} setCart={setCart} />} />
-          <Route path="/search/:term" element={<SearchItem cart={cart} setCart={setCart} />} />
-          <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
-          <Route path='/techfaq' element={<TechFaq/>}/>
-          <Route path='/paymentfaq' element={<PaymentFaq/>}/>
-          <Route path='/iosfaq' element={<IosFaq/>}/>
-          <Route path='/andriodfaq' element={<AndroidFaq/>}/>
-          </Routes>
-          <Footer />
-      </Router>
-      
-    </>
+    <Router>
+      <Navbar cart={cart} setData={setData} />
+
+      <Routes>
+        <Route path="/" element={<Product cart={cart} setCart={setCart} items={data} />} />
+        <Route path="/product/:id" element={<ProductDetail cart={cart} setCart={setCart} />} />
+        <Route path="/search/:term" element={<SearchItem cart={cart} setCart={setCart} />} />
+        <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+
+        <Route path="/techfaq" element={<TechFaq />} />
+        <Route path="/paymentfaq" element={<PaymentFaq />} />
+        <Route path="/iosfaq" element={<IosFaq />} />
+        <Route path="/andriodfaq" element={<AndroidFaq />} />
+
+        <Route path="/pagination" element={<Pagination />} />
+
+        <Route path="/dropdown/news" element={<News />} />
+        <Route path="/dropdown/blogs" element={<Blogs />} />
+        <Route path="/dropdown/infographics" element={<Infographics />} />
+        <Route path="/dropdown/testimonials" element={<Testimonials />} />
+      </Routes>
+
+      <Footer />
+    </Router>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,66 +7,56 @@ function Footer() {
       <div className="container">
         <div className="row">
 
-          {/* Brand Info */}
           <div className="col-md-3 mb-4">
-            <h5 className="text-uppercase">ElectroShop</h5>
+            <h5>ElectroShop</h5>
             <p>
-              Your one-stop destination for the latest electronics, gadgets,
-              and accessories at the best prices.
+              Your one-stop destination for the latest electronics and gadgets.
             </p>
           </div>
 
-          {/* Customer Service */}
           <div className="col-md-2 mb-4">
-            <h6 className="text-uppercase">Customer Service</h6>
+            <h6>Customer Service</h6>
             <ul className="list-unstyled">
-              <li><a href="#" className="text-light text-decoration-none">Help Center</a></li>
-              <li><a href="#" className="text-light text-decoration-none">Returns</a></li>
-              <li><a href="#" className="text-light text-decoration-none">Warranty</a></li>
-              <li><a href="#" className="text-light text-decoration-none">Track Order</a></li>
+              <li><a href="#" className="text-light">Help Center</a></li>
+              <li><a href="#" className="text-light">Returns</a></li>
+              <li><a href="#" className="text-light">Warranty</a></li>
+              <li><a href="#" className="text-light">Track Order</a></li>
             </ul>
           </div>
 
-          {/* Quick Links */}
           <div className="col-md-2 mb-4">
-            <h6 className="text-uppercase">Quick Links</h6>
+            <h6>Quick Links</h6>
             <ul className="list-unstyled">
-              <li><a href="#" className="text-light text-decoration-none">Mobiles</a></li>
-              <li><a href="#" className="text-light text-decoration-none">Laptops</a></li>
-              <li><a href="#" className="text-light text-decoration-none">Accessories</a></li>
-              <li><a href="#" className="text-light text-decoration-none">Offers</a></li>
+              <li>
+                <Link to="/pagination" className="text-light text-decoration-none">
+                  Service
+                </Link>
+              </li>
             </ul>
           </div>
 
-          {/* FAQ Section */}
           <div className="col-md-2 mb-4">
-            <h6 className="text-uppercase">FAQs</h6>
+            <h6>FAQs</h6>
             <ul className="list-unstyled">
-              <li><Link to="/techfaq" className="text-light text-decoration-none">Technology</Link></li>
-              <li><a href="/iosfaq" className="text-light text-decoration-none">Ios Version</a></li>
-              <li><a href="/andriodfaq" className="text-light text-decoration-none">Android Version</a></li>
-               <li><Link to="/paymentfaq" className="text-light text-decoration-none">Payment Options</Link></li>
-             
+              <li><Link to="/techfaq" className="text-light">Technology</Link></li>
+              <li><Link to="/iosfaq" className="text-light">iOS Version</Link></li>
+              <li><Link to="/andriodfaq" className="text-light">Android Version</Link></li>
+              <li><Link to="/paymentfaq" className="text-light">Payment</Link></li>
             </ul>
           </div>
 
-          {/* Contact */}
           <div className="col-md-3 mb-4">
-            <h6 className="text-uppercase">Contact Us</h6>
+            <h6>Contact</h6>
             <p>Email: support@electroshop.com</p>
             <p>Phone: +91 XXXXX 43210</p>
-            <p>Location: Chadni Chowk, Delhi, India</p>
+            <p>Delhi, India</p>
           </div>
-
         </div>
 
-        <hr className="border-secondary" />
+        <hr />
 
-        {/* Bottom */}
         <div className="text-center pb-3">
-          <p className="mb-0">
-            © {new Date().getFullYear()} ElectroShop. All Rights Reserved.
-          </p>
+          © {new Date().getFullYear()} ElectroShop
         </div>
       </div>
     </footer>
